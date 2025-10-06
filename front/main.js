ac33d1ed5876c99b8ca233b1f09869daa3b4b748
Léa <lea.ballester@epitech.eu>
2025-10-06 16:54:32 +0200

    [INI] Initialisation projet

---
+++ front/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.data("helloComponent", () => ({
+    message: "Loading...",
+    init() {
+      fetch("http://localhost:8080/api/hello")
+        .then((res) => res.text())
+        .then((data) => (this.message = data))
+        .catch((err) => (this.message = "Error fetching from back"));
+    },
+  }));
+});
