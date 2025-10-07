b1c9ce6a0587e2970bbb789413c3f3cda346c0a9
Léa <lea.ballester@epitech.eu>
2025-10-07 11:07:39 +0200

    [API] Connexion API avec le back

---
+++ back/src/main/java/com/example/demo/ApiController.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
+
+@RestController
+public class ApiController {
+
+    @Value("${betaseries.api.key}")
+    private String apiKey;
+
+    @RequestMapping("/test")
+    public String test() {
+        return "Testing API controller!";
+    }
+
+    @GetMapping(value = "/callclienttest")
+    private String getTestClient() {
+        String uri = "http://localhost:8080/test";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        return result;
+    }
+
+    @GetMapping(value = "/shows")
+    public Object getShows() {
+        String url = "https://api.betaseries.com/shows/list";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-BetaSeries-Key", apiKey);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Object> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                entity,
+                Object.class);
+        return response.getBody();
+    }
+}
